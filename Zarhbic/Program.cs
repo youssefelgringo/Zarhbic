d293462b236e57e2ff6f7947950b4635090e2022
youssefelgringo <youssefcoco800@gmail.com>
2024-01-17 18:03:30 +0100

    caca

--- Zarhbic/Program.cs
+++ Zarhbic/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-internal class Program
+class ZarhbicCalculator
 {
     static void Main()
     {
@@ -11,20 +11,36 @@ internal class Program
         // Saisie de la chaîne de calcul depuis la console
         string input = Console.ReadLine();
 
-        // Appel du service de gestion des erreurs et d'évaluation
-        string resultat = GestionErreursEtEvaluation(input);
+        // Vérification si l'entrée n'est pas nulle ou vide
+        if (!string.IsNullOrEmpty(input))
+        {
+            // Appel du service de gestion des erreurs et d'évaluation
+            string resultat = GestionErreursEtEvaluation(input);
 
-        // Affichage du résultat ou du message d'erreur
-        Console.WriteLine($"Résultat : {resultat}");
+            // Affichage du résultat ou du message d'erreur
+            Console.WriteLine($"Résultat : {resultat}");
+        }
+        else
+        {
+            Console.WriteLine("Erreur : La chaîne de calcul est nulle ou vide.");
+        }
     }
 
     static string GestionErreursEtEvaluation(string expression)
     {
         try
         {
-            // Logique d'évaluation de l'expression RPN
-            double resultat = EvaluerExpressionRPN(expression);
-            return resultat.ToString();
+            // Vérification si l'expression n'est pas nulle ou vide
+            if (!string.IsNullOrEmpty(expression))
+            {
+                // Logique d'évaluation de l'expression RPN
+                double resultat = EvaluerExpressionRPN(expression);
+                return resultat.ToString();
+            }
+            else
+            {
+                return "Erreur : L'expression de calcul est nulle ou vide.";
+            }
         }
         catch (Exception ex)
         {
@@ -51,30 +67,53 @@ internal class Program
             else
             {
                 // Si le token est un opérateur, effectuer l'opération correspondante
-                double operand2 = pile.Pop();
-                double operand1 = pile.Pop();
-
-                switch (token)
+                if (pile.Count >= 2)
                 {
-                    case "+":
-                        pile.Push(operand1 + operand2);
-                        break;
-                    case "-":
-                        pile.Push(operand1 - operand2);
-                        break;
-                    case "*":
-                        pile.Push(operand1 * operand2);
-                        break;
-                    case "/":
-                        pile.Push(operand1 / operand2);
-                        break;
-                    default:
-                        throw new ArgumentException($"Opérateur non pris en charge : {token}");
+                    double operand2 = pile.Pop();
+                    double operand1 = pile.Pop();
+
+                    switch (token)
+                    {
+                        case "+":
+                            pile.Push(operand1 + operand2);
+                            break;
+                        case "-":
+                            pile.Push(operand1 - operand2);
+                            break;
+                        case "*":
+                            pile.Push(operand1 * operand2);
+                            break;
+                        case "/":
+                            // Vérification pour éviter la division par zéro
+                            if (operand2 != 0)
+                            {
+                                pile.Push(operand1 / operand2);
+                            }
+                            else
+                            {
+                                throw new ArgumentException("Division par zéro.");
+                            }
+                            break;
+                        default:
+                            throw new ArgumentException($"Opérateur non pris en charge : {token}");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Nombre insuffisant d'opérandes pour l'opération.");
                 }
             }
         }
 
-        // Le résultat final doit être le seul élément restant dans la pile
-        return pile.Pop();
+        // Vérification si la pile contient un seul élément
+        if (pile.Count == 1)
+        {
+            // Le résultat final doit être le seul élément restant dans la pile
+            return pile.Pop();
+        }
+        else
+        {
+            throw new ArgumentException("Expression invalide : nombre incorrect d'opérandes.");
+        }
     }
 }
